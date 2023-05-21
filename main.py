1c7d672ed955856a5d115751f82707c7cafd2651
Shreeram Dhimal <69409610+shree-dhimal@users.noreply.github.com>
2023-05-21 22:51:09 +0545

    Final-Commit

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ import datetime
 import os
 import csv
 
-
 # Get the screen resolution
 user32 = ctypes.windll.user32
 screen_width = user32.GetSystemMetrics(0)
@@ -18,7 +17,7 @@ screen_height = user32.GetSystemMetrics(1)
 
 # Create a Tkinter window
 root = tk.Tk()
-root.title("Real-Time Population Visulization")
+root.title("Real-Time Population Visualization")
 root.geometry(f"{screen_width}x{screen_height}")
 root.configure(bg="white")
 root.resizable(False, False)
@@ -52,6 +51,10 @@ def add_data(x, y):
     # Update the plot on the canvas
     canvas.draw()
 
+    # Keep the last data point visible on the graph
+    ax.relim()
+    ax.autoscale_view()
+
 def animate():
     global current_index
 
@@ -70,39 +73,44 @@ def animate():
     current_index += 1  # Increment the current index
 
     # Schedule the next animation
-    root.after(1000, animate)
+    root.after(5000, animate)
 
 def update_chart():
     if textbox_country.index("end") == 0:
-        tk.messagebox.showinfo("Empty Text Box",  "Please Enter The Country Code to Continue!")
+        tk.messagebox.showinfo("Empty Text Box", "Please Enter The Country Code to Continue!")
     else:
         write_csv()
         # Clear the existing data
         x_vals.clear()
         y_vals.clear()
-            
+
         # Reset the current index to start from the same spot
         global current_index
         current_index = 0
+
         # Set the animation flag to True
         global animation_running
         animation_running = True
+
         country_name = textbox_country.get()
         label_static_country.config(text=f"Country Name: {country_name.upper()}")
 
         # Start the animation
         animate()
         textbox_country.delete(0, tk.END)
-        
+
 def stop_animation():
     global animation_running
     animation_running = False
 
+    # Update the country name
+    country_name = textbox_country.get()
+    label_static_country.config(text=f"Country Name: {country_name.upper()}")
 
-
+    # Clear the textbox
+    textbox_country.delete(0, tk.END)
 
 def get_country_population(country_code, year):
-    
     # Your World Bank Open Data API key
     api_key = "YOUR_API_KEY"
 
@@ -116,23 +124,16 @@ def get_country_population(country_code, year):
         # Parse the JSON response
         data = response.json()
         try:
-        # Extract the population value
+            # Extract the population value
             population = data[1][0]['value']
             return year, population
         except:
-            tk.messagebox.showinfo("ERROR",  "The Country Code you provided doesnt have the data or you have enterred wrong Country code, please try again")
+            tk.messagebox.showinfo("ERROR", "The Country Code you provided doesn't have the data or you have entered the wrong Country code, please try again")
             return None
-            
 
-        # # Print the population information
-        # print(f"Country Code: {country_code}")
-        # print(f"Year: {year}")
-        # print(f"Population: {population}")
     else:
-        tk.messagebox.showinfo("ERROR",  "The Country Code you provided doesnt have the data or you have enterred wrong Country code, please try again")
+        tk.messagebox.showinfo("ERROR", "The Country Code you provided doesn't have the data or you have entered the wrong Country code, please try again")
         return None
-
-# Get the population of a specific country (e.g., "USA" for United States) for a specific year (e.g., 2021)
 
 def write_csv():
     x = datetime.datetime.now()
@@ -143,24 +144,21 @@ def write_csv():
     data_list = []
 
     if get_country_population(country_name, init_year) is None:
-        tk.messagebox.showinfo("ERROR",  "The Country Code you provided doesnt have the data or you have enterred wrong Country code, please try again")
+        tk.messagebox.showinfo("ERROR", "The Country Code you provided doesn't have the data or you have entered the wrong Country code, please try again")
     else:
         while init_year < this_year:
             year, population = get_country_population(country_name, init_year)
             data_list.append([year, population])
             init_year += 1
-    
+
     file_path = 'data_stream.csv'
     file_exists = os.path.isfile(file_path)
 
     with open(file_path, 'w', newline='') as file:
         writer = csv.writer(file)
-
         writer.writerow(['Year', 'Population'])
         writer.writerows(data_list)
         return True
-
-
 
 # Create a label for the static country name
 label_static_country = tk.Label(root, text="Country Name:", font=("Arial", 12), bg="white")
@@ -174,11 +172,11 @@ canvas.get_tk_widget().pack(pady=10)
 button_frame = tk.Frame(root, bg="white")
 button_frame.pack(pady=(10, 0))
 
-# Create a label for country name
+# Create a label for the country name
 label_country = tk.Label(button_frame, text="Enter Country Name:", font=("Arial", 12), bg="white")
 label_country.grid(row=0, column=0, padx=10, pady=5)
 
-# Create a textbox for country name
+# Create a textbox for the country name
 textbox_country = tk.Entry(button_frame, font=("Arial", 12), width=20)
 textbox_country.grid(row=0, column=1, padx=10, pady=5)
 
